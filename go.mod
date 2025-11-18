7ea699c090615cf0136144f7bbf378aa01030b2f
Muhammad Azwin Rifai <azwinrifai321@gmail.com>
2025-11-18 21:29:52 +0700

    i hope this was right
    -

---
+++ go.mod
@@ -0,0 +1,3 @@
+module sistem-tarif-parkir
+
+go 1.25.3
---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import "fmt"
+
+
+func cekJamLebih(jamParkir int) int {
+
+	var jamParkirLebih int
+
+    if jamParkir > 2 {
+        jamParkirLebih = jamParkir - 2
+    } else {
+        jamParkirLebih = 0
+    }
+    return jamParkirLebih
+}
+
+func hitungBiayaParkir(jamParkir int, statusMember bool, hariLibur bool) int {
+
+	tarifDasar := 5000
+	tarifJamKetiga := 2000
+	tarifHariLibur := 3000
+	totalBiaya := 0
+
+	if jamParkir <= 2 {
+		totalBiaya = tarifDasar
+	}else{
+		totalBiaya = tarifDasar + (tarifJamKetiga * cekJamLebih(jamParkir))
+	}
+
+	if statusMember == true{
+		if jamParkir <=5{
+			totalBiaya = totalBiaya - (totalBiaya * 50 / 100)
+		}else{
+			totalBiaya = totalBiaya - (totalBiaya * 30 / 100)
+		}
+		
+	}
+
+	if hariLibur == true{
+		totalBiaya+=tarifHariLibur
+	}
+
+	fmt.Print("Total Biaya Parkir: ", totalBiaya, "\n")
+	return totalBiaya
+}
+
+func main() {
+    // Test case 1: 4 jam, bukan member, bukan hari libur
+    hitungBiayaParkir(4, false, false)
+    
+    // Test case 2: 2 jam, member, hari libur
+    hitungBiayaParkir(2, true, true)
+}
